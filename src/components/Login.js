9f0a3c8f2751d2ffcb2edcc8e04e1acec7ca78ec
maheshwarisomya <som.maheshwari2002@gmail.com>
2023-05-06 18:35:51 +0530

    update page

--- src/components/Login.js
+++ src/components/Login.js
@@ -40,7 +40,9 @@ const Login = () => {
     <div className='main-body'>
     <div className="container-sign">
       <h2 className="h2-container">{isSignUp ? 'Sign Up' : 'Login'}</h2>
-      
+      {isSignUp && (
+        <p>Create an account to access exclusive features.</p>
+      )}
         <form className="form-sign" onSubmit={handleSubmit}>
         <input
          className='login-input'
